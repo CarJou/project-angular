22e59b1f675edaf10a023487fa83aa8286e71e3f
CarJou <carlajoumier@gmail.com>
2021-09-16 22:17:12 -0300

    edit styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Curso de Angular';
+  // title = 'Curso de Angular';
   course = 'Spring y Angular';
   teacher = 'Anya Taylor';
 }
--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -5,6 +5,7 @@ import { ClientService } from './client.service';
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
+  styleUrls: ['./client.component.css'],
 })
 export class ClientComponent implements OnInit {
   clients?: Client[];
